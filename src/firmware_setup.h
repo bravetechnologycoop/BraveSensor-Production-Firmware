35da8600117aaeedbf2a437c44a6e6421d609d1f
Heidi <hfedorak@gmail.com>
2021-01-24 19:04:55 -0800

    setup firmware complete.  Needs official versioning somehow...

---
+++ src/firmware_setup.h
@@ -0,0 +1,102 @@
+/*
+ * Project:  Programmer input/setup
+ *
+ * Description:
+ *
+ *  This file contains all BraveSensor #define macros that
+ *  must be set by a human during BraveSensor setup, for example device ID's,
+ *  Particle wifi credentials, etc etc.
+ *
+ *  This file also contains macros for code execution options. For example
+ *  using debug messages displayed to a serial port or not, running Particle
+ *  device in manual mode or not, etc etc.
+ *
+ * Directions:
+ *
+ *  This file must be included in every .cpp file, and in the .ino file.
+ *  Normally global header files would be included via gcc's AC_CONFIG_HEADERS
+ *  or AH_HEADER macro. I, however, FLAT OUT REFUSE to screw with the Particle
+ *  makefiles! I don't want to be stuck with broken code and a broken toolchain
+ *  for the next six months!!  grrrrrrr.....
+ *
+ * Author: Heidi Fedorak
+ * Date:  August 2020
+ *
+ */
+#ifndef FIRMWARE_SETUP_H
+#define FIRMWARE_SETUP_H
+
+//*************global settings*****************
+
+//delcare what log level you want for setup firmware:
+
+#define DEBUG_LEVEL LOG_LEVEL_WARN
+
+//declare what type of device you are setting up. Uncomment the 
+//appropriate define, do not set it to anything
+
+#define XETHRU_PARTICLE
+//#define IM21_PARTICLE
+//#define INS3331_PARTICLE
+//#define XM132_PARTICLE
+
+//***********Device Info**************************
+
+//pay attention to data type (string vs int) and size constraints:
+
+//always use the "" string format for strings
+//string max length is 63 chars + null char = 64 chars
+//do not leave anything undefined, empty char arrays will throw error
+//device ID must be an int
+
+#define LOCATIONID "Heidi"           
+#define DEVICETYPE "RadarArgon"     
+#define DEVICEID 5  //int             
+
+
+//*************Wifi Settings*******************
+
+//see documentation for more info on when and how to use this
+
+//pay attention to the following size and type constraints:
+
+//always use the "" string format
+//Do not leave any SSID or password undefined: empty elements in char array will throw an error
+//Strings are limited to 62 characters each: 1st char reserved for array index (see changeSSID cloud function)
+//and last char is for null char at the end of the string.
+
+#define CLIENTSSID0 "Testbed"
+#define CLIENTSSID1 "ssid"
+#define CLIENTSSID2 "ssid"
+#define CLIENTSSID3 "ssid"
+
+
+#define CLIENTPWD0 "fireweed"
+#define CLIENTPWD1 "fireweed2"
+#define CLIENTPWD2 "password"
+#define CLIENTPWD3 "password"
+
+//*************Bluetooth Door Sensor Settings***********
+
+//pay attention to the following data type and endianness directions:
+
+//door ID for each individual IM21 broadcast in advertising data is 3 bytes
+//found on sticker on button: bottom row of numbers/letters, take the first
+//three bytes listed and enter them in reverse order. For example if bottom
+//row of numbers/letters on sticker is 1a2b3c45, door ID will be 3c 2b 1a.
+
+#define DOORID_BYTE1 0x32   //unsigned char in hex
+#define DOORID_BYTE2 0xE8   //unsigned char in hex
+#define DOORID_BYTE3 0x51   //unsigned char in hex
+
+
+//************XeThru Settings**************
+
+// make sure you enter a setting of the appropriate type (float vs int)
+#define XETHRU_LED_SETTING 0            //int
+#define XETHRU_NOISEMAP_SETTING 0       //int
+#define XETHRU_SENSITIVITY_SETTING 5    //int
+#define XETHRU_MIN_DETECT_SETTING 0.5   //float
+#define XETHRU_MAX_DETECT_SETTING 4.0   //float
+
+#endif
---
+++ src/setupFirmware.cpp
@@ -0,0 +1,124 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/home/heidi/Programming/particleProgramming/BraveSensorProductionFirmware/src/setupFirmware.ino"
+/*
+ * Project BraveSensorProductionFirmware
+ * 
+ * Description: Particle Argon/Photon firmware for Brave
+ *              ODetect project.
+ * 
+ * Author(s): Sampath Satti, Wayne Ng, Sajan Rajdev, Heidi Fedorak
+ * 
+ * Sampath Sattie, Wayne Ng, Sajan Rajdev - wrote original XeThru code
+ * 
+ * Heidi Fedorak - re-wrote XeThru code to be more scalable, and
+ *                 and added remote wifi creds update and IM21 
+ *                 BLE door sensor features.
+ *
+ * 
+ */
+
+#include "firmware_setup.h"
+
+//**********FLASH ADDRESSES***********************
+//wifi
+void setup();
+void loop();
+#line 22 "/home/heidi/Programming/particleProgramming/BraveSensorProductionFirmware/src/setupFirmware.ino"
+#define ADDR_SSIDS 0   		  		    	//sizeof = 320
+#define ADDR_PWDS 320			        		//sizeof = 320
+#define ADDR_WIFI_CONNECT_LOG 640			//sizeof = 4
+
+//xethru
+#define ADDR_XETHRU_LED 644		      	//sizeof = 4
+#define ADDR_XETHRU_NOISEMAP 648      //sizeof = 4
+#define ADDR_XETHRU_SENSITIVITY 652   //sizeof = 4
+#define ADDR_XETHRU_MIN_DETECT 656    //sizeof = 4
+#define ADDR_XETHRU_MAX_DETECT 660    //sizeof = 4
+
+//im21 door sensor
+#define ADDR_IM21_DOORID 664		    	//sizeof = 3
+
+//general device settings
+#define ADDR_LOCATION_ID 667          //sizeof = 64
+#define ADDR_DEVICE_ID 731            //sizeof = 4
+#define ADDR_DEVICE_TYPE 735          //sizeof = 64
+
+//next available memory location is 735+64 = 799
+
+
+//*************************System/Startup messages for Particle API***********
+
+SerialLogHandler LogHandler(DEBUG_LEVEL);
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+
+  //write wifi SSIDs and passwords to flash
+  char mySSIDs[5][64] = {CLIENTSSID0, CLIENTSSID1, CLIENTSSID2, CLIENTSSID3, "thisisastringwith63characterslalalalalalalalalalalalalalalala63"};
+  char myPasswords[5][64] = {CLIENTPWD0, CLIENTPWD1, CLIENTPWD2, CLIENTPWD3, "cowardlyarchaiccorp"};
+  EEPROM.put(ADDR_SSIDS,mySSIDs);  
+  EEPROM.put(ADDR_PWDS,myPasswords);
+
+  //store the number of times the Argon has connected to wifi 
+  int wifiLogCount = 0;
+  EEPROM.put(ADDR_WIFI_CONNECT_LOG,wifiLogCount);  
+
+
+  //write general device settings to flash
+  char locationID[64] = LOCATIONID;
+  char deviceType[64] = DEVICETYPE;
+  int deviceID = DEVICEID;
+
+  EEPROM.put(ADDR_LOCATION_ID, locationID);
+  EEPROM.put(ADDR_DEVICE_TYPE, deviceType);
+  EEPROM.put(ADDR_DEVICE_ID, deviceID);
+
+
+
+  #if defined(XETHRU_PARTICLE)
+  //load settings into variable of the appropriate type instead of trusting
+  //EEPROM.put constructor to get it right. If I want min_detect to be a 
+  //4 byte float and put() types it as an 8 byte double, it will over-write
+  //other areas of memory meant for other variables
+  int led = XETHRU_LED_SETTING; 
+  int noisemap = XETHRU_NOISEMAP_SETTING;
+  int sensitivity = XETHRU_SENSITIVITY_SETTING;
+  float min_detect = XETHRU_MIN_DETECT_SETTING;
+  float max_detect = XETHRU_MAX_DETECT_SETTING;
+
+  EEPROM.put(ADDR_XETHRU_LED, led);  
+  EEPROM.put(ADDR_XETHRU_NOISEMAP, noisemap);  
+  EEPROM.put(ADDR_XETHRU_SENSITIVITY, sensitivity);  
+  EEPROM.put(ADDR_XETHRU_MIN_DETECT, min_detect);  
+  EEPROM.put(ADDR_XETHRU_MAX_DETECT, max_detect);  
+  #endif
+
+  #if defined(IM21_PARTICLE)
+
+  unsigned char doorbyte1 = DOORID_BYTE1;
+  unsigned char doorbyte2 = DOORID_BYTE2;
+  unsigned char doorbyte3 = DOORID_BYTE3;
+
+  EEPROM.put(ADDR_IM21_DOORID, doorbyte1);
+  EEPROM.put((ADDR_IM21_DOORID+1), doorbyte2);
+  EEPROM.put((ADDR_IM21_DOORID+2), doorbyte3);
+  #endif
+
+  Log.warn("Setup Complete");
+
+
+
+}  //end setup()
+
+// loop() runs over and over again, as quickly as it can execute.
+// it is the arduino substitute for while(1) in main()
+void loop() {
+
+}
+
+
+
