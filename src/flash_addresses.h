1b2f7fb926bcbec50c0cacba8a0e7c034b9cb1de
Mario Cimet <mario@brave.coop>
2021-05-12 14:50:22 -0700

    wip

--- src/flash_addresses.h
+++ src/flash_addresses.h
@@ -1,30 +1,44 @@
-#ifndef FLASH_ADDRESSES_H
-#define FLASH_ADDRESSES_H
+/*
+ *
+ * Brave firmware state machine for single Argon
+ * written by Heidi Fedorak/Mario Cimet, Apr 2021
+ * 
+ *  Addresses for where in EEPROM certain variables are written.
+ *  All except the flags are values that initialize to defaults 
+ *  when firmware is first flashed, and can later be changed using
+ *  console functions.
+ *
+*/
+
+#ifndef FLASHADDRESSES_H
+#define FLASHADDRESSES_H
 
 //**********FLASH ADDRESSES***********************
 //wifi
 #define ADDR_SSIDS 0   		  		    	//sizeof = 320
-#define ADDR_PWDS 320			        	//sizeof = 320
-#define ADDR_WIFI_CONNECT_LOG 640			//sizeof = 4
+#define ADDR_PWDS 320                           //sizeof = 320
+#define ADDR_WIFI_DISCONNECT_LOG 640		    //sizeof = 4
 
-//xethru
-#define ADDR_XETHRU_LED 644		      	//sizeof = 4
-#define ADDR_XETHRU_NOISEMAP 648      //sizeof = 4
-#define ADDR_XETHRU_SENSITIVITY 652   //sizeof = 4
-#define ADDR_XETHRU_MIN_DETECT 656    //sizeof = 4
-#define ADDR_XETHRU_MAX_DETECT 660    //sizeof = 4
+//write originals flag
+//flags are uint16_t so 2 bytes each
+#define ADDR_INITIALIZE_SM_CONSTS_FLAG 644  //uint16_t = 2 bytes
+#define ADDR_INITIALIZE_DOOR_ID_FLAG 646    //uint16_t = 2 bytes
 
-//im21 door sensor
-#define ADDR_IM21_DOORID 664		    	//sizeof = 3
+//state machine constants
+//all are uint32_t so 4 bytes each
+#define ADDR_INS_THRESHOLD 648   	  		    	
+#define ADDR_STATE1_MAX_TIME 652			        	
+#define ADDR_STATE2_MAX_DURATION 656		
+#define ADDR_STATE3_MAX_STILLNES_TIME 660
 
-//general device settings
-#define ADDR_LOCATION_ID 667          //sizeof = 64
-#define ADDR_DEVICE_ID 731            //sizeof = 4
-#define ADDR_DEVICE_TYPE 735          //sizeof = 64
+//im21 door sensor ID
+//struct with three unsigned chars (uint8_t)
+#define ADDR_IM21_DOORID 664	
 
-#define ADDR_PASSWORD_FOR_SSIDS 799        //sizeof = 64
-#define ADDR_PASSWORD_FOR_PASSWORDS 863    //sizeof = 64
+//next available address is 667
+#define ADDR_PASSWORD_FOR_SSIDS 667        //sizeof = 64
+#define ADDR_PASSWORD_FOR_PASSWORDS 731    //sizeof = 64
 
-//next available memory location is 863+64 = 927
+//next available memory location is 795
 
 #endif
