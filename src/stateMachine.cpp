d790e493d0d16b1450c2b31809313c3bed0f07c7
James Seto <jameseto1@gmail.com>
2021-06-09 14:18:04 -0700

    fix duration timer bug

--- src/stateMachine.cpp
+++ src/stateMachine.cpp
@@ -233,8 +233,6 @@ void state3_stillness(){
 
     Log.warn("motion spotted again, going from state3_stillness to state2_duration");
     publishStateTransition(3, 2, checkDoor.doorStatus, checkINS.iAverage);
-    //zero the duration timer
-    state2_duration_timer = millis();
     //go back to state 2, duration
     stateHandler = state2_duration;
 
@@ -245,6 +243,14 @@ void state3_stillness(){
     publishStateTransition(3, 0, checkDoor.doorStatus, checkINS.iAverage);
     stateHandler = state0_idle;
 
+  }
+  else if(millis() - state2_duration_timer >= state2_max_duration){
+
+    Log.warn("See duration alert, going from state3 to idle after alert publish");
+    publishStateTransition(3, 0, checkDoor.doorStatus, checkINS.iAverage);
+    Log.error("Duration Alert!!");
+    Particle.publish("Duration Alert", "duration alert", PRIVATE);
+    stateHandler = state0_idle;
   }
   else if(millis() - state3_stillness_timer >= state3_max_stillness_time){
 
