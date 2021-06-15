faff35ec94c14717b62450aefd3f94a79942ffd8
James Seto <jameseto1@gmail.com>
2021-06-15 13:36:44 -0700

    changed positions of eeprom to enable transfer of stored values from prod version

--- src/flashAddresses.h
+++ src/flashAddresses.h
@@ -21,30 +21,30 @@
 #define ADDR_PWDS 320                           //sizeof = 320
 #define ADDR_WIFI_DISCONNECT_LOG 640		    //sizeof = 4
 
-//write originals flag
-//flags are uint16_t so 2 bytes each
-#define ADDR_INITIALIZE_SM_CONSTS_FLAG 642  //uint16_t = 2 bytes
-#define ADDR_INITIALIZE_DOOR_ID_FLAG 644    //uint16_t = 2 bytes
-
-//state machine constants
-//all are uint32_t so 4 bytes each
-#define ADDR_SLOW_THRESHOLD 648   	  		    	
-#define ADDR_STATE1_MAX_TIME 652			        	
-#define ADDR_STATE2_MAX_DURATION 656		
-#define ADDR_STATE3_MAX_STILLNES_TIME 660
+//xethru
+#define ADDR_XETHRU_LED 644		      //sizeof = 4
+#define ADDR_XETHRU_NOISEMAP 648      //sizeof = 4
+#define ADDR_XETHRU_SENSITIVITY 652   //sizeof = 4
+#define ADDR_XETHRU_MIN_DETECT 656    //sizeof = 4
+#define ADDR_XETHRU_MAX_DETECT 660    //sizeof = 4
 
 //im21 door sensor ID
 //struct with three unsigned chars (uint8_t)
-#define ADDR_IM21_DOORID 664	
+#define ADDR_IM21_DOORID 664
 
-//xethru
-#define ADDR_XETHRU_LED 667		      	//sizeof = 4
-#define ADDR_XETHRU_NOISEMAP 671      //sizeof = 4
-#define ADDR_XETHRU_SENSITIVITY 675   //sizeof = 4
-#define ADDR_XETHRU_MIN_DETECT 679    //sizeof = 4
-#define ADDR_XETHRU_MAX_DETECT 683    //sizeof = 4
+//write originals flag
+//flags are uint16_t so 2 bytes each
+#define ADDR_INITIALIZE_XETHRU_CONSTS_FLAG 667  //uint16_t = 2 bytes
+#define ADDR_INITIALIZE_SM_CONSTS_FLAG 669      //uint16_t = 2 bytes
+#define ADDR_INITIALIZE_DOOR_ID_FLAG 671        //uint16_t = 2 bytes
 
-#define ADDR_INITIALIZE_XETHRU_CONSTS_FLAG 687 //sizeof = 2
-//next available address is 691
+//state machine constants
+//all are uint32_t so 4 bytes each
+#define ADDR_SLOW_THRESHOLD 673   	  		    	
+#define ADDR_STATE1_MAX_TIME 677			        	
+#define ADDR_STATE2_MAX_DURATION 681		
+#define ADDR_STATE3_MAX_STILLNES_TIME 685
+
+//next available address is 689
 
 #endif
