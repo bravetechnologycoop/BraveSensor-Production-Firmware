9a9e144b2b7c81eaf9dec6cda888a4213413b30a
elektradl <elektrad@students.cs.ubc.ca>
2021-09-24 14:04:53 -0700

    refactored code to add consoleFunctions

---
+++ src/consoleFunctionsslp.h
@@ -0,0 +1,35 @@
+/*
+ * HEADER
+ * Console functions for Particle Argon firmware for sound & light prototype
+ * based off of 
+ * sound-light-prototype by:
+ *			  Blake Shular
+ *            Maria Phelan
+ *
+*/
+
+#ifndef CONSOLEFUNCTIONSSLP_H
+#define CONSOLEFUNCTIONSSLP_H
+
+//*************************macro defines**********************************
+
+
+//******************global variable declarations*******************
+
+//*************************function declarations*************************
+
+//setup() functions 
+
+void setupConsoleFunctions();
+int change_timer_length(String command);
+#line 12 "c:/Users/phela/brave/traffic_light/BraveSensor-Production-Firmware/src/sound-light-prototype.ino"
+#define BUZZER D6
+#define BUTTON D5
+
+int start_siren(String command); // cloud function
+void button_interrupt();
+void timer_overflow();
+void publish_messages();
+
+int flag = 0;
+Timer timer(5000, timer_overflow, true);
